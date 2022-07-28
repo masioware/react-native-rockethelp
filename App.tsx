b4236f1f19123ba404cd7351b7699d2b3c69d653
masioware <marcioedumartinez@gmail.com>
2022-07-28 15:19:07 -0300

    Class 1 content

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/Loading";
+
+import { SignIn } from "./src/screens/SignIn";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
